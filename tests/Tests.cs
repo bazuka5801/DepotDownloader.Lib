92be7fa5ba17fbfab5b0119cf1c027315afaa913
Maxim Bazuev <bazuka5801@mail.ru>
2018-09-20 17:44:58 +0300

    Create Test with using new events

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -16,7 +16,7 @@ namespace DepotDonwloader.Lib.Tests
         private string _directory = Path.GetDirectoryName(Assembly.GetAssembly(typeof(global::DepotDownloader.DepotDownloader)).Location);
         
         [Test]
-        public void Test1()
+        public void Test_RustDownloading()
         {
             Directory.SetCurrentDirectory(_directory);
             _config = new DownloadConfig()
@@ -33,6 +33,30 @@ namespace DepotDonwloader.Lib.Tests
             
             downloader.ClearCache();
             Assert.IsFalse(Directory.Exists(".temp\\.DepotDownloader"));
+            Directory.Delete(".temp", true);
+        }
+        
+        [Test]
+        public void Test_RustDownloadingWithLogger()
+        {
+            Directory.SetCurrentDirectory(_directory);
+            _config = new DownloadConfig()
+            {
+                AppID = 258550,
+                DownloadAllPlatforms = false,
+                InstallDirectory = ".temp",
+                UsingFileList = true,
+                FilesToDownloadRegex = new List<Regex>() { _rustFilesRegex },
+                SavePathProcessor = SavePathProcessor
+            };
+            _config.OnMessageEvent += (type, message) => Console.WriteLine($"[{type}] {message}");
+            _config.OnReportProgressEvent += (message) => Console.WriteLine($"[Progress] {message}");
+            var downloader = new global::DepotDownloader.DepotDownloader(_config);
+            downloader.Download(true);
+            
+            downloader.ClearCache();
+            Assert.IsFalse(Directory.Exists(".temp\\.DepotDownloader"));
+            Directory.Delete(".temp", true);
         }
 
         private string SavePathProcessor(string path)
